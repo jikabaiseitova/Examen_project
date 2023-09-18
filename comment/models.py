78e082e18f4c7ee90287e40b10604d120072bc93
Admin <jika.baiseitova@gmail.com>
2023-09-18 15:01:52 +0600

    models

---
+++ comment/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+import telebot
+
+User = get_user_model()
+
+bot = telebot.TeleBot('6107937292:AAF6sGWwygfT-YunuLvalkRhtJ-w9_RVmb4', parse_mode=None)
+
+
+class Forum(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Forum"
+        verbose_name_plural = "Forums"
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return f"{self.text[:20]} ..."
+
+
+
---
+++ veb_forum/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VebForumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'veb_forum'
